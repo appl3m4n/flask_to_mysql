f9c0adaffa885befc8cda8c221d4d0b5220cf75c
Roman Hrdlovic <109904342+appl3m4n@users.noreply.github.com>
2024-03-18 22:13:02 +0100

    xx

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
@@ -10,29 +10,24 @@ app.config['MYSQL_DB'] = "users_db"
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
+    userDetails = None  # Define userDetails initially as None
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name,email) VALUES (%s,%s)",(username,email))
+        cur.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (username, email))
         mysql.connection.commit()
         cur.close()
 
-        #return "success"
-
-    return render_template('index.html')
-
-@app.route('/x', methods=['GET','POST'])
-def users():
-    cur = mysql.connection.cursor()
-    users = cur.execute("SELECT * FROM users")
+    cur2 = mysql.connection.cursor()
+    users = cur2.execute("SELECT * FROM users")
     if users > 0:
-        userDetails = cur.fetchall()
-    return render_template('index.html',userDetails=userDetails)
-    
+        userDetails = cur2.fetchall()
+
+    return render_template('index.html', userDetails=userDetails)
+
 if __name__ == "__main__":
     app.run(debug=True)
